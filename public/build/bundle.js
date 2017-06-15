2cf04b137325c5d44da6ea1f43462cc058e2416b
Andrii <andreysalim@hotmail.com>
2017-06-15 14:04:19 +0300

    Everything is working

--- public/build/bundle.js
+++ public/build/bundle.js
@@ -72,5 +72,8 @@
 
 console.log('hi');
 
+console.log(`string text line 1
+string text line 2`);
+
 /***/ })
 /******/ ]);
--- src/main.js
+++ src/main.js
@@ -1,1 +1,4 @@
 console.log('hi');
+
+console.log(`string text line 1
+string text line 2`);
